ae6b1c76a385dee955ee306d6667f24dbd1c4a30
11aghayan <11.aghayan.99@gmail.com>
2024-09-22 23:44:19 +0400

    database starting queries added; basic types added

---
+++ server/src/db/database.sql
@@ -0,0 +1,166 @@
+ROLLBACK;
+BEGIN;
+-- EXTENSIONS
+CREATE EXTENSION "uuid-ossp";
+
+CREATE EXTENSION CITEXT;
+
+-- DOMAINS
+CREATE DOMAIN ID AS 
+  UUID DEFAULT uuid_generate_v4() NOT NULL;
+
+CREATE DOMAIN EMAIL AS CITEXT
+  CHECK (value ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$');
+
+CREATE DOMAIN SEX AS CHAR(1)
+  CHECK (value ~* '^m$|^f$');
+
+CREATE DOMAIN PERMISSION AS CHAR(1) NOT NULL
+  CHECK(value ~* '^s$|^f$|^r$');
+
+CREATE DOMAIN SEASON AS CHAR(2)
+  CHECK(value ~* '^sp$|^su$|^au$|^wi$|^al$');
+
+CREATE DOMAIN TRIP_TYPE AS CHAR(3)
+  CHECK(value ~* '^rel$|^sig$|^act$|^cul$|^par$');
+
+CREATE DOMAIN PRIORITY AS CHAR(1)
+  CHECK(value ~* '^h$|^m$|^l$');
+
+CREATE DOMAIN COVER_FOR AS CHAR(1)
+  CHECK(value ~* '^d$|^t$');
+
+CREATE DOMAIN COVER_POS AS CHAR(1)
+  CHECK(value ~* '^l$|^m$|^r$');
+
+CREATE DOMAIN RATING_SCORE AS SMALLINT(2)
+  CHECK(value >= 1 AND value <= 50);
+
+-- TABLES
+CREATE TABLE user_tbl (
+  id ID PRIMARY KEY,
+  email EMAIL UNIQUE NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  birthday DATE,
+  sex SEX,
+  join_date DATE DEFAULT now() NOT NULL
+);
+
+CREATE TABLE password_tbl (
+  user_id UUID UNIQUE NOT NULL,
+  hash VARCHAR(60) NOT NULL,
+
+  CONSTRAINT fk_user
+    FOREIGN KEY(user_id)
+    REFERENCES user_tbl(id) 
+    ON DELETE CASCADE
+);
+
+CREATE TABLE admin_tbl (
+  id ID PRIMARY KEY,
+  username VARCHAR(20) UNIQUE NOT NULL,
+  permission PERMISSION,
+  password_hash VARCHAR(60) NOT NULL
+); 
+
+CREATE TABLE profile_photo_tbl (
+  user_id UUID UNIQUE NOT NULL,
+  file TEXT NOT NULL,
+
+  CONSTRAINT fk_user
+    FOREIGN KEY(user_id)
+    REFERENCES user_tbl(id) 
+    ON DELETE CASCADE
+);
+
+CREATE TABLE trip_tbl (
+  id ID PRIMARY KEY,
+  user_id UUID NOT NULL,
+  label VARCHAR(50) NOT NULL,
+  view_count BIGINT DEFAULT 0 NOT NULL,
+  cost INT,
+  description TEXT NOT NULL,
+  season SEASON[] NOT NULL,
+  avg_rating RATING_SCORE,
+  duration_from SMALLINT NOT NULL,
+  duration_to SMALLINT NOT NULL,
+  type TRIP_TYPE NOT NULL,
+
+
+  CONSTRAINT fk_user
+    FOREIGN KEY(user_id)
+    REFERENCES user_tbl(id) 
+    ON DELETE CASCADE
+);
+
+CREATE TABLE picked_by_us_trip_tbl(
+  trip_id UUID UNIQUE NOT NULL,
+
+  CONSTRAINT fk_trip
+    FOREIGN KEY(trip_id)
+    REFERENCES trip_tbl(id) 
+    ON DELETE CASCADE
+);
+
+CREATE TABLE destination_tbl(
+  id ID PRIMARY KEY,
+  label VARCHAR(50) NOT NULL,
+  trip_id UUID NOT NULL,
+  parent_destination_id UUID,
+  priority PRIORITY DEFAULT 'm' NOT NULL,
+  description TEXT NOT NULL,
+  staying_cost INT,
+  visiting_cost INT,
+  stay_days SMALLINT NOT NULL,
+  visiting_time_from SMALLINT,
+  visiting_time_to SMALLINT, 
+
+
+  CONSTRAINT fk_trip
+    FOREIGN KEY(trip_id)
+    REFERENCES trip_tbl(id)
+    ON DELETE CASCADE,
+  CONSTRAINT fk_parent
+    FOREIGN KEY(parent_destination_id)
+    REFERENCES destination_tbl(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE photo_tbl(
+  id ID PRIMARY KEY,
+  file TEXT NOT NULL,
+  trip_id UUID NOT NULL,
+  destination_id UUID,
+  cover_for COVER_FOR,
+  cover_pos COVER_POS,
+
+  CONSTRAINT fk_trip 
+    FOREIGN KEY(trip_id)
+    REFERENCES trip_tbl(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE rating_tbl(
+  id ID PRIMARY KEY,
+  trip_id UUID NOT NULL,
+  user_id UUID NOT NULL,
+  comment TEXT,
+  score RATING_SCORE,
+  replied_to UUID,
+  like_count INT DEFAULT 0 NOT NULL,
+  dislike_count INT DEFAULT 0 NOT NULL,
+
+  CONSTRAINT fk_trip
+    FOREIGN KEY(trip_id)
+    REFERENCES trip_tbl(id)
+    ON DELETE CASCADE,
+  CONSTRAINT fk_user
+    FOREIGN KEY(user_id)
+    REFERENCES user_tbl(id),
+  CONSTRAINT fk_reply
+    FOREIGN KEY(replied_to)
+    REFERENCES rating_tbl(id)
+    ON DELETE CASCADE
+);
+
+COMMIT;
--- server/src/types.ts
+++ server/src/types.ts
@@ -9,3 +9,89 @@ declare namespace NodeJS {
     REFRESH_TOKEN_SECRET: string;
   }
 }
+
+type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N ? Acc[number] : Enumerate<N, [...Acc, Acc['length']]>;
+type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>;
+
+export type T_Sex = 'm' | 'f';
+export type T_Permission = 's' | 'f' | 'r';
+export type T_Season = 'sp' | 'su' | 'au' | 'wi' | 'al';
+export type T_Trip_Type = 'rel' | 'sig' | 'act' | 'cul' | 'par';
+export type T_Priority = 'h' | 'm' | 'l';
+export type T_Cover_For = 't' | 'd';
+export type T_Cover_Pos = 'l' | 'm' | 'r';
+export type T_Rating_Score = IntRange<1, 50>;
+
+export type T_User = {
+  id: string;
+  email: string;
+  name: string;
+  birthday?: string;
+  sex?: T_Sex;
+  join_date: string;
+};
+
+export type T_Password = {
+  user_id: string;
+  hash: string;
+};
+
+export type T_Admin = {
+  id: string;
+  username: string;
+  permission: T_Permission;
+  password_hash: string;
+};
+
+export type T_Profile_Photo = {
+  user_id: string;
+  file: string;
+};
+
+export type T_Trip = {
+  id: string;
+  user_id: string;
+  label: string;
+  view_count: number;
+  cost?: number;
+  description: string;
+  season: T_Season[];
+  avg_rating?: number;
+  duration_from: number;
+  duration_to: number;
+  type: T_Trip_Type;
+};
+
+export type T_Destination = {
+  id: string;
+  label: string;
+  trip_id: string;
+  parent_destination_id?: string;
+  priority: T_Priority;
+  description: string;
+  staying_cost?: number;
+  visiting_cost?: number;
+  stay_days: number;
+  visiting_time_from?: number;
+  visiting_time_to?: number;
+};
+
+export type T_Photo = {
+  id: string;
+  file: string;
+  trip_id: string;
+  destination_id?: string;
+  cover_for?: T_Cover_For;
+  cover_pos?: T_Cover_Pos
+};
+
+export type T_Rating = {
+  id: string;
+  trip_id: string;
+  user_id: string;
+  comment?: string;
+  score: T_Rating_Score;
+  replied_to?: string;
+  like_count: number;
+  dislike_count: number;
+};
